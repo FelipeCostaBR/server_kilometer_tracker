837f864305ee37c446e27062774c9ae0b47095a3
Felipe <felipejsilvacosta@gmail.com>
2022-05-20 19:46:43 +1000

    refactoring user router

--- server_etros_kilometer_track/src/routes/users.routes.ts
+++ server_etros_kilometer_track/src/routes/users.routes.ts
@@ -13,16 +13,23 @@ import UpdateUserService from '../services/UserServices/UpdateUserService';
 const usersRouter = Router();
 
 
-usersRouter.get('/',
-  // [ensureAuthenticated, isAdmin],
-  async (request: Request, response: Response) => {
-    const usersRepository = getRepository(User);
-    const user = await usersRepository.find().catch(error => {
-      throw new AppError(error);
-    });
-
-    response.json({ user });
+usersRouter.get('/', [ensureAuthenticated, isAdmin], async (request: Request, response: Response) => {
+  const usersRepository = getRepository(User);
+  const user = await usersRepository.find().catch(error => {
+    throw new AppError(error);
   });
+
+  response.json({ user });
+});
+
+usersRouter.get('/:id', async (request: Request, response: Response) => {
+  const { id } = request.params;
+
+  const usersRepository = getRepository(User);
+  const user = await usersRepository.findOne(id);
+
+  response.json(user);
+});
 
 usersRouter.post('/', async (request: Request, response: Response) => {
   const userData = request.body;
@@ -36,32 +43,28 @@ usersRouter.post('/', async (request: Request, response: Response) => {
   return response.json(user);
 });
 
-usersRouter.delete('/:id',
-  // [ensureAuthenticated, isAdmin],
-  async (request: Request, response: Response) => {
-    const { id } = request.params;
+usersRouter.delete('/:id', [ensureAuthenticated, isAdmin], async (request: Request, response: Response) => {
+  const { id } = request.params;
 
-    const deleteUserService = new DeleteUserService();
+  const deleteUserService = new DeleteUserService();
 
-    const user = await deleteUserService.execute(id).catch(error => {
-      throw new AppError(error);
-    });
-    return response.json(user);
+  const user = await deleteUserService.execute(id).catch(error => {
+    throw new AppError(error);
+  });
+  return response.json(user);
+});
+
+usersRouter.put('/:id', [ensureAuthenticated, isAdmin], async (request: Request, response: Response) => {
+  const { id } = request.params;
+  const userData = request.body;
+
+  const updateUserService = new UpdateUserService();
+
+  const user = await updateUserService.execute(id, userData).catch(error => {
+    throw new AppError(error);
   });
 
-usersRouter.put('/:id',
-  // [ensureAuthenticated, isAdmin],
-  async (request: Request, response: Response) => {
-    const { id } = request.params;
-    const userData = request.body;
-
-    const updateUserService = new UpdateUserService();
-
-    const user = await updateUserService.execute(id, userData).catch(error => {
-      throw new AppError(error);
-    });
-
-    return response.json(user);
-  });
+  return response.json(user);
+});
 
 export default usersRouter;
