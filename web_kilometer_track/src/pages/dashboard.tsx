6415080daa0724c53561ae22ae3ff16f04e85966
Felipe <felipejsilvacosta@gmail.com>
2022-05-25 22:09:50 +1000

    improve odometerReader input

--- web_kilometer_track/src/pages/dashboard.tsx
+++ web_kilometer_track/src/pages/dashboard.tsx
@@ -1,11 +1,12 @@
-import { Box, Input, InputGroup, InputLeftElement, Text, Flex, Button, Stack } from '@chakra-ui/react';
+import { Box, Text, Flex, Button, Stack } from '@chakra-ui/react';
+import React from 'react';
 import { Card } from '../components/Card';
 import { Header } from '../components/Header';
-import InputMask from "react-input-mask";
+import { OdometerReader } from '../components/OdometerReader';
 
-import { IoSpeedometerOutline } from 'react-icons/io5';
+export default function Dashboard() {
+  const [value, setValue] = React.useState()
 
-export default function Login() {
   return (
     <Flex
       w='100vw'
@@ -37,26 +38,7 @@ export default function Login() {
           mt={4}
           as='form'
         >
-          <InputGroup>
-            <Input
-              as={InputMask}
-              placeholder='Type here'
-              _placeholder={{ opacity: 0.5, color: 'inherit' }}
-              paddingLeft={14}
-              size='lg'
-              focusBorderColor='green.light'
-              bg='cardBg.dark'
-              color='white'
-              border='InactiveBorder'
-              isRequired={true}
-              mask="***,***.**"
-              maskChar={null}
-            />
-            <InputLeftElement w='4.5rem' h='100%' className="InputLeft" pointerEvents="none">
-              <IoSpeedometerOutline color='white' size={24} />
-            </InputLeftElement>
-
-          </InputGroup>
+          <OdometerReader />
           <Button
             type={'submit'}
             w='100%'
